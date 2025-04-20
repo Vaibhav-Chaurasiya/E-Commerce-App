8add43eab1a62d703eab5c54b6821b0867a5a43f
Vaibhav Chaurasiya <vaibhavchaurasiya50@gmail.com>
2025-04-21 00:19:58 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import ProductDetails from './pages/ProductDetails';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
